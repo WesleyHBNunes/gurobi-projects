d748f5f9bf6afdac79070956a497fa98a9f144bf
Wesley Henrique Batista Nunes <wesley.rn2@gmail.com>
2019-07-11 22:33:25 -0300

    Add sys args on getting file

--- bin-packing/main.py
+++ bin-packing/main.py
@@ -1,9 +1,10 @@
 import parser
 import model
+import sys
 
 
 def main():
-    items, total_weight = parser.read_file("instances/knapPI_3_10000_1000_1")
+    items, total_weight = parser.read_file("instances/" + sys.argv[1])
     objective_function, m = model.solve(items, total_weight)
     model.print_used_vars(m)
     print("Objective Function: " + str(objective_function))
