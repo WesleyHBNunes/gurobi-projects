6ebbef0ef34c5beb4ab52220376cf37ed151829e
wesley <wesley.rn2@gmail.com>
2019-10-02 11:39:42 -0300

    Initial commit of facility location problem

---
+++ facility-location-problem/model.py
@@ -0,0 +1,34 @@
+from gurobi import *
+
+
+def solve():
+    env = Env()
+    env.setParam('TimeLimit', 3600)
+    model = Model('Attribution Problem model', env)
+    add_vars(model)
+    set_objective_function(model)
+    add_constraints(model)
+    model.update()
+    write_model(model)
+    model.optimize()
+    return model.getObjective().getValue(), model
+
+
+def add_vars(model):
+    pass
+
+
+def set_objective_function(model):
+    pass
+
+
+def add_constraints(model):
+    pass
+
+
+def write_model(model):
+    model.write('model.lp')
+
+
+def print_used_vars(model):
+    pass
---
+++ facility-location-problem/parser.py
@@ -0,0 +1,4 @@
+def read_file(file):
+    file = open(file, "r")
+    lines = file.readlines()
+    pass
