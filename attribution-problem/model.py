314e3f529ff1f72676f417719ce9718f19dde720
wesley <wesley.rn2@gmail.com>
2019-10-02 11:35:05 -0300

    Nome of model updated on attribution problem

--- attribution-problem/model.py
+++ attribution-problem/model.py
@@ -4,7 +4,7 @@ from gurobi import *
 def solve(coast, n):
     env = Env()
     env.setParam('TimeLimit', 3600)
-    model = Model('Bin Packing Model', env)
+    model = Model('Attribution Problem model', env)
     x = add_vars(model, n)
     set_objective_function(model, x, coast, n)
     add_constraints(model, x, n)
